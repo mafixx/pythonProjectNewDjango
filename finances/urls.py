aba4d4add53154288cbb4480666d9955d10a2fbc
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-12-13 22:18:47 -0300

    commit

--- finances/urls.py
+++ finances/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from .views import index
+from .views import index, transacoes_por_usuario, contas_por_usuario
 
 app_name = "finances"
 
 urlpatterns = [
-    path("", index, name="index")
-
+    path("", index, name="index"),
+    path(
+        "<int:user_id>/transacoes",
+        transacoes_por_usuario,
+        name="transacoes_por_usuario"
+    ),
+    path(
+        "<int:user_id>/contas",
+        contas_por_usuario,
+        name="contas_por_usuario"
+    )
 ]
--- finances/views.py
+++ finances/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 
+from .models import Transacao, ContaFinanceira
+
 
 def index(request):
-    return render(request, "finances/index.html")
+
+    num_transacoes = Transacao.objects.filter(
+        conta_debito__usuario=request.user
+    ).count()
+
+    num_contas = ContaFinanceira.objects.filter(
+        usuario=request.user
+    ).count()
+
+    context = {
+        "num_transacoes": num_transacoes,
+        "num_contas": 2
+    }
+
+    return render(
+        request, "finances/index.html", context
+    )
+
+def transacoes_por_usuario(request, user_id):
+
+    return render(request, "financas/transacoes_por_usuario.html")
+def contas_por_usuario(request, user_id):
+
+    return render(request, "finances/contas_por_usuario.html")
+

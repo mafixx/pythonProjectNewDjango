814d60bead635426b1076b91653f4f43e8b552a2
Pedro Barbieri <pedro.romaguera@hotmail.com>
2022-12-14 22:12:17 -0300

    commit

--- finances/urls.py
+++ finances/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views import index, transacoes_por_usuario, contas_por_usuario
+from .views import (
+    index,
+    transacoes_por_usuario,
+    contas_por_usuario,
+    nova_conta,
+    nova_transacao
+)
 
 app_name = "finances"
 
@@ -15,5 +21,15 @@ urlpatterns = [
         "<int:user_id>/contas",
         contas_por_usuario,
         name="contas_por_usuario"
+    ),
+    path(
+        "<int:user_id>/contas/nova",
+        nova_conta,
+        name="nova_conta"
+    ),
+    path(
+        "<int:user_id>/transacoes/nova",
+        nova_transacao,
+        name="nova_transacao"
     )
 ]
